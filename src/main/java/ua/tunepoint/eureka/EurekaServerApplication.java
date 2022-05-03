c11d26f4b7fb622d62d410d5be117553d4db8763
Kovalev Danil <opana.loog@gmail.com>
2022-05-03 19:57:21 +0300

    fix version and packages

--- src/main/java/ua/tunepoint/eureka/EurekaServerApplication.java
+++ src/main/java/ua/tunepoint/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package ua.tunepoint.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ua/tunepoint/eureka/EurekaServerApplicationTests.java
+++ src/test/java/ua/tunepoint/eureka/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package ua.tunepoint.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
